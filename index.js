4a50574ea1ddf3d16717a28e5597bc9b2a4afa9d
AlanDeveloper <alanssantos32@gmail.com>
2022-02-05 19:33:44 -0300

    Add comments

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+require('dotenv').config(); // Habilita variáveis de ambiente
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false })); // Declara qual biblioteca será utilizada para fazer o parsing
+app.use(express.json()); // Transforma solicitações em JSON
+app.use(cors()); // Habilita o CORS
 
 let PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
